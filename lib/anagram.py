0d0308ba7d19064bf27d16355c9a0b61d2a20c86
dennis mukuria <denismukuria@gmail.com>
2025-04-16 22:43:21 +0300

    complete all tasks

--- lib/anagram.py
+++ lib/anagram.py
@@ -1,1 +1,8 @@
 # your code goes here!
+class Anagram:
+    def __init__(self, word):
+        self.word = word
+
+    def match(self, l):
+        results = [i for i in l if sorted(list(i)) == sorted(list(self.word))]
+        return results
